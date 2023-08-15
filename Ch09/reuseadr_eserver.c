b6237e57b05968fc8f9189b25366e1db1aa21b88
chao <xiaochao200@126.com>
2023-08-15 20:24:39 +0800

    add Ch09

---
+++ Ch09/reuseadr_eserver.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+
+#define TRUE 1
+#define FALSE 0
+
+int main(int argc, char* argv[])
+{
+    int serv_sock, clnt_sock;
+    char message[30];
+    int option, str_len;
+    socklen_t optlen, clnt_adr_sz;
+    struct sockaddr_in serv_adr, clnt_adr;
+
+    serv_sock = socket(PF_INET, SOCK_STREAM, 0);
+
+    // optlen = sizeof(option);
+    // option = TRUE;
+    // setsockopt(serv_sock, SOL_SOCKET, SO_REUSEADDR, (void*)&option, optlen);
+
+    memset(&serv_adr, 0, sizeof(serv_adr));
+    serv_adr.sin_addr.s_addr = htonl(INADDR_ANY);
+    serv_adr.sin_family = AF_INET;
+    serv_adr.sin_port = htons(atoi(argv[1]));
+
+    bind(serv_sock, (struct sockaddr*)&serv_adr, sizeof(serv_adr));
+    listen(serv_sock, 5);
+
+    clnt_adr_sz = sizeof(clnt_adr);
+    clnt_sock = accept(serv_sock, (struct sockaddr*)&clnt_adr, &clnt_adr_sz);
+
+    while ((str_len=read(clnt_sock, message, sizeof(message))))
+    {
+        write(clnt_sock, message, str_len);
+        write(1, message, str_len);
+    }
+    close(clnt_sock);
+    close(serv_sock);
+    
+    return 0;
+}
